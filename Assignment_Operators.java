53f328b032f304d11a7a2b7df831776f24da3d8f
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2024-01-07 00:59:48 +0530

    Add files via upload

---
+++ Assignment_Operators.java
@@ -0,0 +1,26 @@
+public class Assignment_Operators {
+    public static void main(String[] args) {
+        // Initialize the variable
+        int num1 = 10;
+
+        // After addition
+        num1 += 2;
+        System.out.println("After addition: num = " + num1);
+
+        // After subtraction
+        num1 -= 4;
+        System.out.println("After subtraction: num = " + num1);
+
+        // After multiplication
+        num1 *= 7;
+        System.out.println("After multiplication: num = " + num1);
+
+        // After division
+        num1 /= 4;
+        System.out.println("After division: num = " + num1);
+
+        // After modulus
+        num1 %= 4;
+        System.out.println("After modulus: num = " + num1);
+    }
+}
---
+++ Relational_Operators.java
@@ -0,0 +1,19 @@
+public class Relational_Operators {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Use relational operators to compare values
+        boolean isEqual = (num1 == num2);
+        boolean isNotEqual = (num1 != num2);
+        boolean isGreaterThan = (num1 > num2);
+        boolean isLessThan = (num1 < num2);
+
+        // Display the output
+        System.out.println("Equal: " + isEqual);
+        System.out.println("Not Equal: " + isNotEqual);
+        System.out.println("num1 is Greater Than num2: " + isGreaterThan);
+        System.out.println("num1 is Less Than num2: " + isLessThan);
+    }
+}
